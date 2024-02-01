51778c4ba49d04f4a329132eab3b6609ac90b57b
Макулбеков Думан Мухтарович <MAKULBED@bank.corp.centercredit.kz>
2024-02-01 08:36:52 +0600

    [chapter 1] 000: add comments

--- src/Example.java
+++ src/Example.java
@@ -1,4 +1,10 @@
+/*
+    Простая программа на Java.
+    Назовите этот файл Example.java.
+*/
+
 class Example {
+    // Программа на Java начинается с вызова main().
     public static void main(String[] args){
         System.out.println("Java движет веб-сетью.");
     }
