0872130b770d0cc2460aedf07223313b4978bfee
Макулбеков Думан Мухтарович <MAKULBED@bank.corp.centercredit.kz>
2024-02-01 08:59:12 +0600

    [chapter 1] 000: new structure and exe 2

--- src/com/javabook/chapter_01/Example_01.java
+++ src/com/javabook/chapter_01/Example_01.java
@@ -1,9 +1,11 @@
+package com.javabook.chapter_01;
+
 /*
     Простая программа на Java.
     Назовите этот файл Example.java.
 */
 
-class Example {
+class Example_01 {
     // Программа на Java начинается с вызова main().
     public static void main(String[] args){
         System.out.println("Java движет веб-сетью.");
---
+++ src/com/javabook/chapter_01/Example_02.java
@@ -0,0 +1,22 @@
+package com.javabook.chapter_01;
+
+/*
+    This demonstrates a variable. / Демонстрация использования переменных
+    Call this file Example2.java. / Назовите этот файл Example2.java.
+*/
+
+class Example2 {
+    public static void main(String[] args) {
+        int myVar1; // this declares a variable / объявление переменной
+        int myVar2; // this declares another variable / объявление еще одной переменной
+
+        myVar1 = 1024; // this assigns 1024 to myVar1 / присваивание переменной myVarl значения 1024
+
+        System.out.println("myVar1 contains " + myVar1);
+
+        myVar2 = myVar1 / 2;
+
+        System.out.print("myVar2 contains myVar1 / 2: ");
+        System.out.println(myVar2);
+    }
+}
