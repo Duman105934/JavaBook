c2a00f696a8adb042cac9c61ef7d5ae2f7851dd4
dumanmakulbekov <makulbekov.d@gmail.com>
2024-02-11 19:08:14 +0600

    [chapter 2] 090: add 1 new class

---
+++ src/com/javabook/chapter_02/listings/$_14_LtoD.java
@@ -0,0 +1,16 @@
+package com.javabook.chapter_02.listings;
+
+public class $_14_LtoD {
+
+    // Демонстрация автоматического преобразования из long в double.
+    public static void main(String[] args) {
+        long L;
+        double D;
+
+        L = 100123285L;
+        D = L;
+
+        System.out.println("L and D: " + L + " " + D);
+
+    }
+}
